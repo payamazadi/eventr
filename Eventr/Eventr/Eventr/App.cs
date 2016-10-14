54fef65e28d6d0c73aab79056f7101090a0247a3
brendancass <brendan.cass@gmail.com>
2016-10-13 20:24:56 -0400

    Main Page UI Mockup

--- Eventr/Eventr/Eventr/App.cs
+++ Eventr/Eventr/Eventr/App.cs
@@ -11,7 +11,58 @@ namespace Eventr
     {
         public App()
         {
-            // The root page of your application
+            var grid = new Grid();
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(5, GridUnitType.Star) });
+
+            var box = new BoxView();
+            box.BackgroundColor = Color.FromHex("#cccdce");
+            grid.Children.Add(box, 1, 0);
+            Grid.SetRowSpan(box, 24);
+
+            var times = new List<dynamic>();
+            for(var i=1; i<=24; i++)
+            {
+                times.Add(new {
+                    time = i + (i < 12 ? "am" : "pm"),
+                    row = i - 1,
+                    col = 0
+                });
+            }
+            foreach(var time in times)
+            {
+                grid.Children.Add(new Label { Text = time.time, BackgroundColor=Color.White }, time.col, time.row);
+                grid.Children.Add(new BoxView { BackgroundColor = Color.White }, 1, time.row);
+            }
+
+            Button eventButton = new Button { Text = "Brunch" };
+            grid.Children.Add(eventButton, 1, 7);
+            Grid.SetRowSpan(eventButton, 4);
+
             var content = new ContentPage
             {
                 Title = "Eventr",
@@ -19,14 +70,14 @@ namespace Eventr
                 {
                     VerticalOptions = LayoutOptions.Center,
                     Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Fuck you in the ass!!"
-                        }
+                        grid
                     }
                 }
             };
 
+            grid.BackgroundColor = Color.White;
+            grid.RowSpacing = 1;
+            grid.ColumnSpacing = 0;
             MainPage = new NavigationPage(content);
         }
 
