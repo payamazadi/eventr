2d208fb0a62a896b7755d827b30b2120baca46db
Brendan Cass <brendan.cass@gmail.com>
2017-11-11 15:19:49 -0500

    created phone input

--- Eventr/App.js
+++ Eventr/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import PhoneInput from "./components/inputs/PhoneInput";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! lol</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text> THere should be something here</Text>
+        <PhoneInput />
       </View>
     );
   }
@@ -16,8 +16,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "purple",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
