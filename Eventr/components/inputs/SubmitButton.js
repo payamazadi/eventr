0da9b913b69d3ded2fb9c35243fff6a89d68f552
Brendan Cass <brendan.cass@gmail.com>
2017-11-28 17:35:50 -0500

    made custom button out of TouchableOpacity

--- Eventr/components/inputs/SubmitButton.js
+++ Eventr/components/inputs/SubmitButton.js
@@ -1,19 +1,28 @@
 import React from "react";
-import { StyleSheet, Button } from "react-native";
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
 
 import Colors from "../../shared/colors";
 
 export default props => {
   return (
-    <Button overrides={{ backgroundColor: Colors.orange }} title="Submit" />
+    <TouchableOpacity style={styles.button} onPress={props.onPress}>
+      <Text style={styles.text}>Submit</Text>
+    </TouchableOpacity>
   );
 };
 
-const styles = {
+const styles = StyleSheet.create({
   button: {
     backgroundColor: Colors.orange,
     borderRadius: 8,
     width: 200,
-    height: 40
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 15
   }
-};
+});
