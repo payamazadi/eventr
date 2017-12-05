bb3d9d3d9af1b5f582ae1c30ed6c7449d4802ae6
Brendan Cass <brendan.cass@gmail.com>
2017-12-05 17:05:35 -0500

    added confirmation input

--- Eventr/App.js
+++ Eventr/App.js
@@ -3,24 +3,20 @@ import { Alert, StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from "expo";
 
 
-import { PhoneInput, SubmitButton } from "inputs";
+import { PhoneInput, ConfirmationInput, SubmitButton } from "inputs";
 import { TextHeading1 } from "text";
 import { colors } from "shared";
 
 export default class App extends React.Component {
   render() {
-    return (
-      <LinearGradient
-        colors={colors.gradient}
-        style={styles.gradient}
-      >
+    return <LinearGradient colors={colors.gradient} style={styles.gradient}>
         <View style={styles.container}>
           <TextHeading1>Welcome to Weaver</TextHeading1>
           <PhoneInput />
-          <SubmitButton onPress = {() => Alert.alert("Next!")} />
+          <ConfirmationInput />
+          <SubmitButton onPress={() => Alert.alert("Next!")} />
         </View>
-      </LinearGradient>
-    );
+      </LinearGradient>;
   }
 } 
 
