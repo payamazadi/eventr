2b61db8db9e0a273027f71ac94f8a209fe936cc9
Brendan Cass <brendan.cass@gmail.com>
2018-01-23 21:10:57 -0500

    added navigation

--- Eventr/App.js
+++ Eventr/App.js
@@ -2,78 +2,91 @@ import React from "react";
 import { Alert, StyleSheet, Text, View } from "react-native";
 import { LinearGradient, Font } from "expo";
 import FontAwesome, { Icons } from "react-native-fontawesome";
- 
-import { PhoneInput, ConfirmationInput, SubmitButton, TextInput, SelectInput} from "inputs";
-import { TextTitle, TextSubtitle, TextHeading1, TextHeading2, TextHeading3, TextCaptionRegular, TextRegular, TextLinkRegular, TextPageTitle } from "text";
+import { StackNavigator, NavigationActions } from "react-navigation";
+
+import Home from './components/pages/Home';
+import Login from "./components/pages/Login";
+import {
+  PhoneInput,
+  ConfirmationInput,
+  SubmitButton,
+  TextInput,
+  SelectInput
+} from "inputs";
+import {
+  TextTitle,
+  TextSubtitle,
+  TextHeading1,
+  TextHeading2,
+  TextHeading3,
+  TextCaptionRegular,
+  TextRegular,
+  TextLinkRegular,
+  TextPageTitle
+} from "text";
 import { colors } from "shared";
 import { Drawer } from "wrappers";
 
+let _container;
+const Navigator = StackNavigator({
+  Home: { screen: Home, header: null },
+  Login: { screen: Login }
+}
+);
+
 export default class App extends React.Component {
-  state = {
-    drawerOpen: false,
-    fontLoaded: false
-  }
+  state = { drawerOpen: false, fontLoaded: false};
+
   async componentDidMount() {
     await Font.loadAsync({
-      "FontAwesome": require("./assets/fonts/fontawesome-webfont.ttf")
+      FontAwesome: require("./assets/fonts/fontawesome-webfont.ttf")
     });
 
-    this.setState({fontLoaded: true});
+    this.setState({ fontLoaded: true });
   }
 
   toggleDrawer(open) {
-    this.setState({drawerOpen: open});
+    this.setState({ drawerOpen: open });
   }
 
   render() {
-    return <LinearGradient colors={colors.gradient} style={styles.gradient}>
-        <View style={styles.container}>
-          <TextCaptionRegular>English (US)</TextCaptionRegular>
-          <TextTitle>Welcome to Weaver</TextTitle>
-          {/*This doesn't stay centered after a certain length..*/}
-          <TextSubtitle>
-            It's easy to start, just enter your phone number
-          </TextSubtitle>
-          <PhoneInput />
-          <ConfirmationInput />
-          <SubmitButton onPress={() => Alert.alert("Next!")} filled>
-            Confirm
-          </SubmitButton>
-          <SubmitButton onPress={() => Alert.alert("Next!")}>
-            Events around me
-          </SubmitButton>
-          <TextInput label="LIST NAME" />
-          <SelectInput label="TYPE" />
-          <TextHeading1>Hello H1</TextHeading1>
-          <TextHeading2>Hello H2</TextHeading2>
-          <TextHeading3>Hello H3</TextHeading3>
-          <TextRegular>Regular text that we'll have everywhere</TextRegular>
-          <TextLinkRegular>
-            Regular link text that we'll have everywhere
-          </TextLinkRegular>
-          <TextPageTitle>Settings</TextPageTitle>
+    return <View colors={colors.gradient} style={styles.gradient}>
+        <Navigator style={styles.container} ref={navigatorRef => {
+            _container = navigatorRef;
+          }} />
+        <View style={styles.navButton}>
           <SubmitButton onPress={() => {
               this.toggleDrawer(true);
-            }} filled>
-            Open the Drawer
+            }}>
+            Open Drawer
           </SubmitButton>
-          <Drawer open={this.state.drawerOpen}>
-            <TextRegular>
-              {this.state.fontLoaded ? (
-                <FontAwesome>{Icons.dashboard}</FontAwesome>
-              ) : null}{" "}
-              Dashboard
-            </TextRegular>
-            <SubmitButton onPress={() => {
-                this.toggleDrawer(false);
-              }}>
-              Close the Drawer
-            </SubmitButton>
-          </Drawer>
         </View>
-      </LinearGradient>;
+        <Drawer open={this.state.drawerOpen}>
+          <TextRegular>
+            {this.state.fontLoaded ? (
+              <FontAwesome>{Icons.dashboard}</FontAwesome>
+            ) : null}{" "}
+            Dashboard
+          </TextRegular>
+          <SubmitButton onPress={() => {
+              this.toggleDrawer(false);
+              //this.props.navigation.navigate("Login");
+              _container.dispatch(NavigationActions.navigate({
+                  type: "Navigation/NAVIGATE",
+                  routeName: "Login"
+                }));
+            }}>
+            other page
+          </SubmitButton>
+          <SubmitButton onPress={() => {
+              this.toggleDrawer(false);
+            }}>
+            Close the Drawer
+          </SubmitButton>
+        </Drawer>
+      </View>;
   }
-} 
+               };
 
 const styles = StyleSheet.create({
   container: {
@@ -84,5 +97,11 @@ const styles = StyleSheet.create({
   gradient: {
     height: "100%",
     width: "100%"
+  },
+  navButton:{
+    position: "absolute",
+    top: 50,
+    right: 10
   }
 });
+
