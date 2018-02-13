0580641cab0d80b7c9fedaf10cde4655b6b8ecc4
Payam Azadi <payamazadi@live.com>
2018-02-13 00:32:16 -0500

    WIP: completing rebase
    wtf is up w merge conflicts? GitHub desktop app just shrugs..no option
    to just choose incoming.. uh?

--- Eventr/App.js
+++ Eventr/App.js
@@ -1,163 +1,44 @@
 import React, { Component } from "react";
-import { Alert, StyleSheet, Text, View } from "react-native";
-import { LinearGradient, Font } from "expo";
-import FontAwesome, { Icons } from "react-native-fontawesome";
-<<<<<<< HEAD
-import { Container, Header, Content, Badge } from 'native-base';
-import { Attendees } from 'lists';
-import { PhoneInput, ConfirmationInput, SubmitButton, TextInput, SelectInput} from "inputs";
-import { TextTitle, TextSubtitle, TextHeading1, TextHeading2, TextHeading3, TextCaptionRegular, TextRegular, TextLinkRegular, TextPageTitle } from "text";
-=======
-import { StackNavigator, NavigationActions } from "react-navigation";
+import { StyleSheet, View } from "react-native";
+import { LinearGradient } from "expo";
 
-import NavigationHelper, { ROUTES } from "./NavigationHelper";
-import Home from './components/pages/Home';
-import Login from "./components/pages/Login";
-import {
-  PhoneInput,
-  ConfirmationInput,
-  SubmitButton,
-  TextInput,
-  SelectInput
-} from "inputs";
-import {
-  TextTitle,
-  TextSubtitle,
-  TextHeading1,
-  TextHeading2,
-  TextHeading3,
-  TextCaptionRegular,
-  TextRegular,
-  TextLinkRegular,
-  TextPageTitle
-} from "text";
->>>>>>> master
+import { StackNavigator, NavigationActions } from "react-navigation";
+import { Provider } from "react-redux";
+
+import { configureStore } from "./store";
+import NavigationHelper from "./NavigationHelper";
+
+import Welcome from "./containers/Welcome";
+import Confirmation from "./containers/Confirmation";
+
+import NavigationDrawer from "./containers/NavigationDrawer";
 import { colors } from "shared";
-import { Drawer } from "wrappers";
 
 let _navContainer;
+const store = configureStore();
 const Navigator = StackNavigator({
-  Home: { screen: Home, header: null },
-  Login: { screen: Login }
-}
-);
+  Welcome: { screen: Welcome },
+  Confirmation: { screen: Confirmation },
+  Attendees: { screen: Attendees }
+});
 
 export default class App extends React.Component {
-  state = { drawerOpen: false, fontLoaded: false};
-
-  async componentDidMount() {
-    await Font.loadAsync({
-      FontAwesome: require("./assets/fonts/fontawesome-webfont.ttf")
-    });
-
-    this.setState({ fontLoaded: true });
-  }
-
-  toggleDrawer(open) {
-    this.setState({ drawerOpen: open });
-  }
-
   render() {
-<<<<<<< HEAD
-    var attendees = [
-        {
-          name: "Brendan Cass",
-          avatar: "brendan.jpg",
-          attending: 1
-        },
-        {
-          name: "Payam Azadi",
-          avatar: "payam.jpg",
-          attending: 2
-        },
-        {
-          name: "Steven Ayers",
-          avatar: "ayers.jpg",
-          attending: 3
-        }
-
-    ];
-
-    return <LinearGradient colors={colors.gradient} style={styles.gradient}>
-        <View style={styles.container}>
-          <TextCaptionRegular>English (US)</TextCaptionRegular>
-          <TextTitle>Welcome to Weaver</TextTitle>
-          {/*This doesn't stay centered after a certain length..*/}
-          <TextSubtitle>
-            It's easy to start, just enter your phone number
-          </TextSubtitle>
-          <PhoneInput />
-          <ConfirmationInput />
-          <SubmitButton onPress={() => Alert.alert("Next!")} filled>
-            Confirm
-          </SubmitButton>
-          <SubmitButton onPress={() => Alert.alert("Next!")}>
-            Events around me
-          </SubmitButton>
-          <TextInput label="LIST NAME" />
-          <SelectInput label="TYPE" />
-          <TextHeading1>Hello H1</TextHeading1>
-          <TextHeading2>Hello H2</TextHeading2>
-          <TextHeading3>Hello H3</TextHeading3>
-          <TextRegular>Regular text that we'll have everywhere</TextRegular>
-          <TextLinkRegular>
-            Regular link text that we'll have everywhere
-          </TextLinkRegular>
-          <TextPageTitle>Settings</TextPageTitle>
-=======
-    return <View colors={colors.gradient} style={styles.gradient}>
-        <Navigator style={styles.container} ref={navigatorRef => {
-            NavigationHelper.NAVIGATOR = navigatorRef;
-          }} />
-        <View style={styles.navButton}>
->>>>>>> master
-          <SubmitButton onPress={() => {
-              this.toggleDrawer(true);
-            }}>
-            Open Drawer
-          </SubmitButton>
-<<<<<<< HEAD
-          <Drawer open={this.state.drawerOpen}>
-            <TextRegular>
-              {this.state.fontLoaded ? (
-                <FontAwesome>{Icons.dashboard}</FontAwesome>
-              ) : null}{" "}
-              Dashboard
-            </TextRegular>
-            <SubmitButton onPress={() => {
-                this.toggleDrawer(false);
-              }}>
-              Close the Drawer
-            </SubmitButton>
-          </Drawer>
-          <Badge><Text>2</Text></Badge>
-          <Attendees attendees={attendees} />
-=======
->>>>>>> master
+    return (
+      <Provider store={store}>
+        <View colors={colors.gradient} style={styles.gradient}>
+          <Navigator
+            style={styles.container}
+            ref={navigatorRef => {
+              NavigationHelper.NAVIGATOR = navigatorRef;
+            }}
+          />
+          <NavigationDrawer />
         </View>
-        <Drawer open={this.state.drawerOpen}>
-          <TextRegular>
-            {this.state.fontLoaded ? (
-              <FontAwesome>{Icons.dashboard}</FontAwesome>
-            ) : null}{" "}
-            Dashboard
-          </TextRegular>
-          <SubmitButton onPress={() => {
-              this.toggleDrawer(false);
-              //this.props.navigation.navigate("Login");
-              NavigationHelper.navigateTo(ROUTES.LOGIN);
-            }}>
-            other page
-          </SubmitButton>
-          <SubmitButton onPress={() => {
-              this.toggleDrawer(false);
-            }}>
-            Close the Drawer
-          </SubmitButton>
-        </Drawer>
-      </View>;
+      </Provider>
+    );
   }
-               };
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -168,11 +49,5 @@ const styles = StyleSheet.create({
   gradient: {
     height: "100%",
     width: "100%"
-  },
-  navButton:{
-    position: "absolute",
-    top: 50,
-    right: 10
   }
 });
-
--- Eventr/NavigationHelper.js
+++ Eventr/NavigationHelper.js
@@ -1,18 +1,21 @@
 import { NavigationActions } from "react-navigation";
 
 export const ROUTES = {
-    HOME:"Home",
-    LOGIN: "Login",
-    ATTENDEES: "Attendees"
-}
+  HOME: "Home",
+  WELCOME: "Welcome",
+  CONFIRMATION: "Confirmation",
+  ATTENDEES: "Attendees"
+};
 
 export default class NavigationHelper {
-    static NAVIGATOR;
+  static NAVIGATOR;
 
-    static navigateTo(route){
-        this.NAVIGATOR.dispatch(NavigationActions.navigate({
-            type: "Navigation/NAVIGATE",
-            routeName: route
-          }));
-    }
+  static navigateTo(route) {
+    this.NAVIGATOR.dispatch(
+      NavigationActions.navigate({
+        type: "Navigation/NAVIGATE",
+        routeName: route
+      })
+    );
+  }
 }
