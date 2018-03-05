082580e3958c0ad96661dcd7d5478770f747fca1
Payam Azadi <payamazadi@live.com>
2018-03-04 21:03:34 -0500

    WIP: pass in an actual list, daft?

--- Eventr/components/pages/AttendeeList.js
+++ Eventr/components/pages/AttendeeList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import FontAwesome, { Icons } from "react-native-fontawesome";
-import { Container, Header, Body, Content } from "native-base";
+import { Container, Header, Body, Content, List, ListItem, Left, Icon } from "native-base";
 import { TransparentOverlay } from "wrappers";
 import { TextRegular } from "text";
 import { Logo } from "common";
 
 export default class AttendeeList extends React.Component {
   static navigationOptions = { header: null };
+  static defaultProps = { attendeeListData: [{name: "wat"}] }
 
   render() {
     const {
@@ -20,7 +21,8 @@ export default class AttendeeList extends React.Component {
         </Header>
 
         <Content>
-          <TextRegular>{this.props.attendeeListData}</TextRegular>
+          
+          <TextRegular>{attendeeListData[0].name}</TextRegular>
         </Content>
         <TransparentOverlay show={false} />
       </Container>
@@ -29,3 +31,4 @@ export default class AttendeeList extends React.Component {
     );
   }
 }
+
--- Eventr/services/index.js
+++ Eventr/services/index.js
@@ -14,10 +14,12 @@ export function loadAttendeeListData(eventId) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (eventId) {
-        resolve({attendeeListId: 1, attendeeListData: "lalal"});
+        resolve({attendeeListId: 1, attendeeListData: 
+          /*[{id: 5, name: "poooooo"}]*/ "kaka"
+        });
       } else {
         reject(new Error("invalid event ID or error"));
       }
-    }, 8000);
+    }, 1000);
   });
 }
