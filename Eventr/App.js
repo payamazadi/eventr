0f4fc873a479badc32bdc777d4a9d60586560c28
brendancass <brendan.cass@gmail.com>
2018-03-12 23:27:45 -0400

    async loading pulled into App container, more event page WIP

--- Eventr/App.js
+++ Eventr/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
-import { LinearGradient } from "expo";
+import { LinearGradient, Font } from "expo";
 
 import { StackNavigator, NavigationActions } from "react-navigation";
 import { Provider } from "react-redux";
@@ -26,8 +26,20 @@ const Navigator = StackNavigator({
 });
 
 export default class App extends React.Component {
+  state = { assetsLoaded: false };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      FontAwesome: require("./assets/fonts/fontawesome-webfont.ttf")
+    });
+
+    this.setState({ assetsLoaded: true });
+  }
+
   render() {
-    return (
+    const { assetsLoaded } = this.state;
+
+    return assetsLoaded ? (
       <Provider store={store}>
         <View colors={colors.gradient} style={styles.gradient}>
           <Navigator
@@ -39,7 +51,7 @@ export default class App extends React.Component {
           <NavigationDrawer />
         </View>
       </Provider>
-    );
+    ) : null; // <--- place holder until we have a loading page
   }
 }
 
--- Eventr/components/pages/EventDisplay.js
+++ Eventr/components/pages/EventDisplay.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { StyleSheet, View, Image } from "react-native";
 import { LinearGradient } from "expo";
-import { TransparentOverlay } from "wrappers";
+import FontAwesome, { Icons } from "react-native-fontawesome";
 
-import { TextHeading3, TextCaptionRegular } from "text";
+import { TransparentOverlay } from "wrappers";
+import { TextTitle, TextHeading1, TextCaptionRegular, TextRegular } from "text";
 import { colors } from "shared";
 
 export default class EventDisplay extends React.Component {
   static navigationOptions = { header: null };
+
+  state = { fontLoaded: false };
 
   render() {
     const { isLoadingEvent, eventData } = this.props;
@@ -15,9 +18,24 @@ export default class EventDisplay extends React.Component {
     return (
       <LinearGradient colors={colors.gradient} style={styles.gradient}>
         <View style={styles.container}>
-          <TextHeading3>
-            Beach Trip <TextCaptionRegular>(Private)</TextCaptionRegular>
-          </TextHeading3>
+          <View style={styles.header}>
+            <TextHeading1>
+              <FontAwesome style={{ fontSize: 52 }}>
+                {Icons.chevronCircleLeft}
+              </FontAwesome>
+            </TextHeading1>
+            <TextHeading1>Event</TextHeading1>
+            <View style={styles.addButtonBorder}>
+              <TextHeading1>
+                <FontAwesome style={styles.addButton}>{Icons.plus}</FontAwesome>
+              </TextHeading1>
+            </View>
+          </View>
+          <View style={styles.body}>
+            <TextHeading1>
+              Beach Trip <TextCaptionRegular>(Private)</TextCaptionRegular>
+            </TextHeading1>
+          </View>
         </View>
         <TransparentOverlay show={isLoadingEvent} />
       </LinearGradient>
@@ -26,20 +44,43 @@ export default class EventDisplay extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
+  container: { height: "100%", width: "100%" },
   gradient: {
     height: "100%",
-    width: "100%"
+    width: "100%",
+    flex: 1,
+    alignItems: "flex-start",
+    justifyContent: "flex-start"
   },
-
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    height: 88,
+    width: "100%",
+    borderBottomWidth: 2,
+    borderStyle: "solid",
+    borderColor: colors.white,
+    marginTop: 16
+  },
+  body: { margin: 16 },
   centerText: {
     textAlign: "center"
   },
   padded: {
     margin: 12
+  },
+  addButtonBorder: {
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: colors.linkBlue,
+    borderRadius: 100,
+    padding: 7,
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  addButton: {
+    color: colors.linkBlue,
+    fontSize: 28
   }
 });
